0b71b6aa0256958c876c530264fe0483ff47d4e0
CavidAbish <cavidabish@gmail.com>
2024-12-17 02:39:38 +0400

    Task14 tapsiriq

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Components/Counter';
+import Timer from './Components/Timer';
+import Watch from './Components/Watch';
+
+function App() {
+  return (
+    <div className="App">
+    <Counter/>
+    <Timer/>
+    <Watch/>
+    </div>
+  );
+}
+
+export default App;
